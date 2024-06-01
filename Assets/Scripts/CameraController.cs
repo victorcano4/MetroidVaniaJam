e652198b79e2995c722a6ba8ea5a218dbf502fba
victorcano4 <victor.cano.rodriguez4@gmail.com>
2024-06-01 16:48:22 +0200

    camera follows player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.position.x,Player.position.y,transform.position.z);
+    }
+}
