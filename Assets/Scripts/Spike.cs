ba47f2b1f78a416459d2d5e87f3e73bcb5a9a322
jrat123456 <142025932+jrat123456@users.noreply.github.com>
2024-06-02 15:53:29 -0400

    Player Health added as instance. Scene modified to be tested with.

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,23 +4,13 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private int trapDamage = 1;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Player takes damage
+            PlayerHealth.instance.TakeDamage(trapDamage);
         }
     }
 }
