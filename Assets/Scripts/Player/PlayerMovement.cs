2be22e9caaf6ae0f5d9e6554bc1e5f3d223b7140
jrat123456 <142025932+jrat123456@users.noreply.github.com>
2024-06-08 13:48:53 -0400

    Bump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,54 +9,90 @@ public class PlayerMovement : MonoBehaviour
     private float slowDownFactor = 0.5f;
     private float jumpForce = 0f;
 
-    private Rigidbody2D myRigidbody;
     public bool IsGrounded;
-    public bool isRunning;
-    public bool isJumping;
+    private bool isRunning;
+    private bool isJumping;
+    private bool isCrouching;
 
     [SerializeField] private Animator player_animator;
     [SerializeField] private SpriteRenderer mySpriteRenderer;
+    private Rigidbody2D myRigidbody;
     private FlipOrientation flipOrientation;
+    private BoxCollider2D myBoxCollider;
+    private Vector2 boxColliderSize;
+    private float shrinkFactor = 0.85f;
+    private float boxColliderShrinkWhileCrouching;
 
-    void Start()
+    private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         flipOrientation = GetComponent<FlipOrientation>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
+        boxColliderSize = myBoxCollider.size;
+        boxColliderShrinkWhileCrouching = boxColliderSize.y * shrinkFactor;
     }
 
     void Update()
-    {  
+    {
         Move();
+        Crouching();
+        HandleColliderSize();
     }
 
-    void Move()
+    private void Crouching()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            isCrouching = true;
+
+        }
+        else
+        {
+            isCrouching = false;
+        }
+    }
+
+    private void Move()
     {
         float moveInput = Input.GetAxis("Horizontal");
         if (moveInput != 0)
         {
             float calculatedMoveSpeed = moveSpeed;
 
-            if ((moveInput > 0 && flipOrientation.PlayerFacingRight() ) || (moveInput < 0 && !flipOrientation.PlayerFacingRight()))
+            if ((moveInput > 0 && flipOrientation.PlayerFacingRight()) || (moveInput < 0 && !flipOrientation.PlayerFacingRight()))
             {
-                calculatedMoveSpeed *= slowDownFactor; // Slow down the player
+                calculatedMoveSpeed *= slowDownFactor;
             }
 
             Vector2 moveVelocity = new Vector2(moveInput * calculatedMoveSpeed, myRigidbody.velocity.y);
             myRigidbody.velocity = moveVelocity;
             isRunning = true;
 
-            //Animation
-            if(player_animator != null) 
+            // Animation
+            if (player_animator != null)
                 player_animator.SetBool("isRunning", true);
         }
-
-        else if (moveInput == 0)
+        else
         {
             isRunning = false;
 
-            //Animation
+            // Animation
             if (player_animator != null)
                 player_animator.SetBool("isRunning", false);
+        }
+    }
+
+    private void HandleColliderSize()
+    {
+        if (isCrouching)
+        {
+            // Shrink the collider
+            myBoxCollider.size = new Vector2(boxColliderSize.x, Mathf.Lerp(myBoxCollider.size.y, boxColliderShrinkWhileCrouching, Time.deltaTime * 10f));
+        }
+        else
+        {
+            // Reset to original size
+            myBoxCollider.size = new Vector2(boxColliderSize.x, Mathf.Lerp(myBoxCollider.size.y, boxColliderSize.y, Time.deltaTime * 10f));
         }
     }
 
@@ -67,7 +103,7 @@ public class PlayerMovement : MonoBehaviour
             IsGrounded = true;
             isJumping = false;
 
-            //Animation
+            // Animation
             if (player_animator != null)
                 player_animator.SetBool("isJumping", false);
         }
@@ -75,16 +111,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        // Changed SpriteRenderer into TilemapRenderer in the if check. Maps are built with tilemaps.
         if (collision.gameObject.CompareTag("Ground") && mySpriteRenderer.bounds.min.y >= collision.gameObject.GetComponent<TilemapRenderer>().bounds.max.y)
         {
             IsGrounded = false;
             isJumping = true;
 
-            //Animation
+            // Animation
             if (player_animator != null)
                 player_animator.SetBool("isJumping", true);
-
         }
     }
 }
